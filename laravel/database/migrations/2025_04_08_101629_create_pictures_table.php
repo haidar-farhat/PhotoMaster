526b91300fd900208f619151d2d05abd7a897e49
farhathaydar7 <haydarfarhatsteam69@gmail.com>
2025-04-08 13:28:37 +0300

    started frontend dev

---
+++ laravel/database/migrations/2025_04_08_101629_create_pictures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('filename');
+            $table->string('local_path');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
